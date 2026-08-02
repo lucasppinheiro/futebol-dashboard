25c05854a4e6c8533f4b76a641a29e9c06945ec5
lucasppinheiro <lucas.gabrielpnr@gmail.com>
2026-08-02 18:59:18 -0300

    fix: serve team crests from local assets

--- club_assets.py
+++ club_assets.py
@@ -2,7 +2,27 @@ from typing import Any
 
 
 ESCUDOS_LOCAIS: dict[str, str] = {
+    "BAH": "static/img/escudos/BAH.jpg",
+    "BOT": "static/img/escudos/BOT.gif",
+    "CAM": "static/img/escudos/CAM.jpg",
     "CAP": "static/img/escudos/CAP.png",
+    "CFC": "static/img/escudos/CFC.jpg",
+    "CHA": "static/img/escudos/CHA.svg",
+    "COR": "static/img/escudos/COR.jpg",
+    "CRU": "static/img/escudos/CRU.gif",
+    "FLA": "static/img/escudos/FLA.gif",
+    "FLU": "static/img/escudos/FLU.gif",
+    "GRE": "static/img/escudos/GRE.jpg",
+    "INT": "static/img/escudos/INT.gif",
+    "MIR": "static/img/escudos/MIR.svg",
+    "PAL": "static/img/escudos/PAL.gif",
+    "RBB": "static/img/escudos/RBB.svg",
+    "REM": "static/img/escudos/REM.svg",
+    "SAN": "static/img/escudos/SAN.gif",
+    "SAO": "static/img/escudos/SAO.jpg",
+    "SPO": "static/img/escudos/SPO.gif",
+    "VAS": "static/img/escudos/VAS.gif",
+    "VIT": "static/img/escudos/VIT.jpg",
 }
 
 
